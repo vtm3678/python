9cc8479b116f666f46a72a760034b5f1a44e453f
vtm <vismitha7906@gmail.com>
2020-03-29 11:52:31 +0530

    boss

---
+++ experiments/wer.py
@@ -0,0 +1,8 @@
+print('who is the actual boss??')
+print('start guessing...')
+while True:
+    line = input('>')
+    if line == 'vtm':
+        break
+    print(line,'!')
+print('Correct answer!!!!!!!!!!!!!!!!!!')
