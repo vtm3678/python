66eabb4fbfaf0125b4c7a5dcb68f1f5901b66ee0
vtm <vismitha7906@gmail.com>
2020-04-01 12:23:01 +0530

    palindrome checker

---
+++ experiments/first.py
@@ -0,0 +1,20 @@
+from xmlrpc.client import boolean
+x = input('type something:')
+l = len(x)
+lastindex = l - 1
+index = 0
+pal = True
+while index < l:
+    forwardchar = x[index]
+    reverseIndex = lastindex - index
+    reverseChar = x[reverseIndex]
+    index = index + 1
+    if forwardchar != reverseChar:
+        pal = False
+        break
+    
+if pal:
+    print(x,' is a palindrome')
+else:
+    print(x,' is not a palindrome')
+    
