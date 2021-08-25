d66686cc448d7db8aab3b7a325bcc69a1994502a
Vishnu TM <62641481+vtm3678@users.noreply.github.com>
2021-08-25 11:06:28 +0530

    deleting wer.py

--- experiments/wer.py
+++
@@ -1,8 +0,0 @@
-print('who is the actual boss??')
-print('start guessing...')
-while True:
-    line = input('>')
-    if line == 'vtm':
-        break
-    print(line,'!')
-print('Correct answer!!!!!!!!!!!!!!!!!!')
